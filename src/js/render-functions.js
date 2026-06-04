65edba5eedd786424934e497e040750c5f803a96
Larimar4you <Zajanezlaja@gmail.com>
2026-06-04 20:56:37 +0200

    GO

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,57 +1,59 @@
-// Импорты
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryEl = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
 
-export function renderGallery(images) {
-  // Очищаем галерею перед добавлением новых изображений
-  clearGallery();
-  galleryEl.innerHTML = images.map(createImageCardMarkup).join('');
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
-  // Инициализируем SimpleLightbox
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+          <li class="gallery-item">
+            <a class="gallery-link" href="${largeImageURL}">
+              <img
+                class="gallery-image"
+                src="${webformatURL}"
+                alt="${tags}"
+              />
+            </a>
+            <div class="info">
+              <p><b>Likes</b><span>${likes}</span></p>
+              <p><b>Views</b><span>${views}</span></p>
+              <p><b>Comments</b><span>${comments}</span></p>
+              <p><b>Downloads</b><span>${downloads}</span></p>
+            </div>
+          </li>
+        `;
+      }
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
 }
 
-function createImageCardMarkup({
-  largeImageURL,
-  webformatURL,
-  tags,
-  likes,
-  views,
-  comments,
-  downloads,
-}) {
-  return `
-    <a href="${largeImageURL}" class="gallery_item">
-      <img src="${webformatURL}" alt="${tags}" class="gallery_image" width="380" loading="lazy"/>
-      <div class="info">
-        <p class="info-item"><b>Likes:</b> ${likes}</p>
-        <p class="info-item"><b>Views:</b> ${views}</p>
-        <p class="info-item"><b>Comments:</b> ${comments}</p>
-        <p class="info-item"><b>Downloads:</b> ${downloads}</p>
-      </div>
-    </a>
-  `;
-}
-
-// Функция очистки галереи
 export function clearGallery() {
-  galleryEl.innerHTML = '';
+  gallery.innerHTML = '';
 }
 
-// Функция отображения сообщения об отсутствии результатов
-export function showNoResultsMessage() {
-  iziToast.info({
-    title: 'Info',
-    message: 'No images found for your search query.',
-    position: 'topRight',
-  });
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
 }
--- src/main.js
+++ src/main.js
@@ -1,51 +1,53 @@
-// Описаний у документаціїimport iziToast from "izitoast";
-// Додатковий імпорт стилівimport "izitoast/dist/css/iziToast.min.css";
-// npm install izitoast --save
-
-// Імпортуємо функціїimport { fetchImages } from "./js/pixabay-api";
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import { fetchImages } from './js/picabay-api';
-import { renderGallery } from './js/render-functions';
-import { showNoResultsMessage } from './js/render-functions';
-import { clearGallery } from './js/render-functions';
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
 
-const loader = document.querySelector('.loader');
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', async event => {
-  // Скасовуємо перезавантаження сторінки
   event.preventDefault();
 
-  const searchQuery = event.target.elements.query.value.trim();
+  const searchQuery = event.target.elements['search-text'].value.trim();
+
   if (!searchQuery) {
-    return showError('Please enter your request');
+    iziToast.error({
+      message: 'Please enter your request',
+      position: 'topRight',
+    });
+    return;
   }
 
   clearGallery();
-  toggleLoader(true);
+  showLoader();
 
   try {
-    const data = await fetchImages(searchQuery);
-    if (data.hits && data.hits.length) {
-      renderGallery(data.hits);
-    } else {
-      showNoResultsMessage();
+    const data = await getImagesByQuery(searchQuery);
+
+    if (data.hits.length === 0) {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+      return;
     }
+
+    createGallery(data.hits);
   } catch (error) {
     console.error(error);
-    showError(`Something went wrong. Please try again!`);
+
+    iziToast.error({
+      message: 'Something went wrong. Please try again!',
+      position: 'topRight',
+    });
   } finally {
-    toggleLoader(false);
+    hideLoader();
   }
 });
-
-function showError(message) {
-  iziToast.error({ title: 'Error', position: 'topRight', message });
-}
-
-// Функція для відображення/приховування індикатора завантаження
-function toggleLoader(isVisible) {
-  loader.style.display = isVisible ? 'block' : 'none';
-}
